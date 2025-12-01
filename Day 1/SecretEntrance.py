fce60a992956c5068245b403a300e93eb6f777a8
Arancibia Leiria Leandro Nicanor <Nicanor95@users.noreply.github.com>
2025-12-01 17:21:55 -0300

    Add day 1, part 2

--- Day 1/SecretEntrance.py
+++ Day 1/SecretEntrance.py
@@ -1,22 +1,49 @@
 import sys
+from math import ceil, floor
 
 if (len(sys.argv) <= 1):
 	print("Missing input.", "Usage: SecretEntrance.py <input> [input ...]", sep="\n")
 	exit(1)
 
+def partOne (lineas: list):
+	position = 50
+	password = 0
+
+	for line in lineas:
+		if (line[0]) == "R":
+			position = (position + int(line[1:])) % 100
+		else:
+			position = (position - int(line[1:])) % 100
+
+		if (position == 0): 
+			password += 1
+
+	return password
+
+def partTwo_brute(lineas: list):
+	position = 50
+	password = 0
+
+	for line in lineas:
+		valor = int(line[1:])
+
+		if (line[0]) == "R":
+			for _ in range(valor):
+				position = (position + 1) % 100
+				password = password + 1 if position == 0 else password
+		else:
+			for _ in range(valor):
+				position = (position - 1) % 100
+				password = password + 1 if position == 0 else password
+	
+	return (password)
+
+
+
 for input_path in sys.argv[1:]:
 	with open(input_path, "r") as infile:
-		position = 50
-		password = 0
-
-		for line in infile.readlines():
-			if (line[0]) == "R":
-				position = (position + int(line[1:])) % 100
-			else:
-				position = (position - int(line[1:])) % 100
-
-			if (position == 0): 
-				password += 1
-
-		print("Password: {}".format(password))
-
+		lines = infile.readlines()
+		print("Password One: {}".format(partOne(lines)), 
+			  "Password Two Brute: {}".format(partTwo_brute(lines)),
+			  sep="\n" )
+		
