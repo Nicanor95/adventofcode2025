58ab48591b1814d7196b04486f81e1e4d3021163
Arancibia Leiria Leandro Nicanor <Nicanor95@users.noreply.github.com>
2025-12-02 03:16:18 -0300

    Add cached version

--- Day 1/SecretEntrance.py
+++ Day 1/SecretEntrance.py
@@ -1,5 +1,6 @@
 import sys
 from math import ceil, floor
+from timeit import default_timer as timer
 
 if (len(sys.argv) <= 1):
 	print("Missing input.", "Usage: SecretEntrance.py <input> [input ...]", sep="\n")
@@ -39,11 +40,50 @@ def partTwo_brute(lineas: list):
 	return (password)
 
 
+def partTwo_brute_cache(lineas: list):
+	position = 50
+	password = 0
+	cache = {}
+
+	for line in lineas:
+		valor = int(line[1:])
+		direccion = line[0]
+		original_pos = position
+		original_pass = password
+
+		# Agregamos el origen al cache si no está
+		if position not in cache:
+			cache.update({position : {}})
+
+		try:
+			cached = cache[position][valor] if direccion == "R" else cache[position][-valor]
+			password += cached[0]
+			position = cached[1]
+		except (KeyError):
+			# No se encuentra en el cache.
+			if direccion == "R":
+				for _ in range(valor):
+					position = (position + 1) % 100
+					password = password + 1 if position == 0 else password
+			else:
+				for _ in range(valor):
+					position = (position - 1) % 100
+					password = password + 1 if position == 0 else password
+				valor = -valor # Invertimos para guardar en cache.
+
+			# Guardamos en cache, para la posicion, guardamos una tupla por cada valor, de forma (delta_password, final_position)
+			cache[original_pos].update({valor : (password - original_pass, position)})
+			
+	return (password)
+
+
 
 for input_path in sys.argv[1:]:
 	with open(input_path, "r") as infile:
 		lines = infile.readlines()
+		
 		print("Password One: {}".format(partOne(lines)), 
 			  "Password Two Brute: {}".format(partTwo_brute(lines)),
+			  "Password Two Cache: {}".format(partTwo_brute_cache(lines)),
 			  sep="\n" )
 		
