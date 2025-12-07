1b5e6dee386a2e8554592cab58697929e38be530
Arancibia Leiria Leandro Nicanor <Nicanor95@users.noreply.github.com>
2025-12-07 19:06:31 -0300

    Add consolidation stuff for Day 5

---
+++ Day 5/Spoils.py
@@ -0,0 +1,42 @@
+import sys
+from functools import reduce
+
+if (len(sys.argv) <= 1):
+	print("Missing input.", "Usage: Spoils.py <input> [input ...]", sep="\n")
+	exit(1)
+
+def consolidable(a: tuple[int,int], b:tuple[int,int]) -> bool:
+	return a[0] <= b[0] <= a[1]
+
+def consolidate(a: tuple[int,int], b:tuple[int,int]):
+	if a[0] <= b[0] <= a[1]:
+		return (a[0],max(b[1],a[1]))
+	return a
+
+for filename in sys.argv[1:]:
+	with open(filename, "r") as spoils:
+		# Read ingredients and fresh list
+		fresh, ingredients = [x.split("\n") for x in spoils.read().split("\n\n")]
+
+		# Translate ingredients to int
+		ingredients = [int(x) for x in ingredients]
+
+		# Capture the ranges into a tuple (start, end) and sort them to ease consolidation
+		fresh = [(int(b),int(c)) for b,c in [x.split('-') for x in fresh]]
+		fresh.sort(key= lambda x: x[0])
+
+		# Consolidate the ranges
+		cFresh = fresh.copy()#list(map(lambda x: consolidate(x[0], x[1]), zip(fresh, fresh[1:])))
+		cFresh = list(zip(cFresh, cFresh[1:]))
+
+		while any(list(map(lambda x: consolidable(x[0], x[1]), cFresh))):
+			cFresh = list(map(lambda x: consolidate(x[0], x[1]), cFresh))
+			print(cFresh)
+			cFresh = list(zip(cFresh, cFresh[1:]))
+
+
+		#for x in cFresh:
+		#	print(x)
+		#print(cFresh)
+		#print(fresh)
+		#print(ingredients)
